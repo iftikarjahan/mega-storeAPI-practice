21a8de12f58151aabe5768a789cffaeb3ab74955
iftikarjahan <iftikarjahan22@gmail.com>
2024-12-29 17:18:38 +0530

    add two controllers in the userController

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,12 +1,20 @@
 const { login } = require("./authController");
+const User = require("../models/User");
+const StatusCodes = require("http-status-codes");
+const {NotFoundError}=require("../errors");
 
-const getAllUsers = (req, res, next) => {
+const getAllUsers = async (req, res, next) => {
   //only allowed by the admin
-  res.send("Get All Users");
+  const users = await User.find({ role: "user" }).select("-password");
+  res.status(StatusCodes.OK).json({ users });
 };
 
-const getSingleUser = (req, res, next) => {    
-  res.send("Get Single User");
+const getSingleUser = async (req, res, next) => {
+  const user = await User.findOne({ _id: req.params.id }).select("-password");
+  if(!user){
+    throw new NotFoundError(`User Not Found wwith the id: ${req.params.id}`);
+  }
+  res.status(StatusCodes.OK).json({ user });
 };
 
 const getCurrentUser = (req, res, next) => {
